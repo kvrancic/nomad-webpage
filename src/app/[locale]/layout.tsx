c4d61f75051c865a0c7f044c7622ce422b25f56f
kvrancic <kvrancic11@gmail.com>
2026-05-27 14:48:05 +0200

    Add Google Analytics (G-BZ3NSKR8S3) and GTM (GTM-P6KQ2LSZ) tracking

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,8 +2,13 @@ import { NextIntlClientProvider } from 'next-intl'
 import { getMessages, setRequestLocale } from 'next-intl/server'
 import { notFound } from 'next/navigation'
 import { Inter, Bebas_Neue } from 'next/font/google'
+import Script from 'next/script'
 import { routing } from '@/i18n/routing'
 import '@/app/globals.css'
+
+// Analytics IDs carried over from the previous site
+const GA_MEASUREMENT_ID = 'G-BZ3NSKR8S3'
+const GTM_ID = 'GTM-P6KQ2LSZ'
 
 const inter = Inter({
   subsets: ['latin', 'latin-ext'],
@@ -40,7 +45,33 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale} className={`${inter.variable} ${bebasNeue.variable}`}>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
+        {/* Google Analytics (gtag.js) */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="ga4" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');`}
+        </Script>
+      </head>
       <body className="font-body bg-white text-anthracite-500 antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
